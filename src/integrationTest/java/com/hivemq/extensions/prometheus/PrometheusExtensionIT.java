b99caee216f74af54bec693cff5bc7613ac8952d
yannick <yannick.weber@hivemq.com>
2022-09-05 14:01:23 +0200

    rename package

--- src/integrationTest/java/com/hivemq/extensions/prometheus/PrometheusExtensionIT.java
+++ src/integrationTest/java/com/hivemq/extensions/prometheus/PrometheusExtensionIT.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.hivemq.extensions.helloworld;
+package com.hivemq.extensions.prometheus;
 
 import com.codahale.metrics.MetricRegistry;
 import com.hivemq.extension.sdk.api.ExtensionMain;
